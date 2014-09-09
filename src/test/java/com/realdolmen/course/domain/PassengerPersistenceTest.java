c36971abe0c11075cc6ae2eb5baaa7367101922b
Aveline Estié <aveline.estie@realdolmen.com>
2014-09-09 14:31:55 +0200

    added unit test for last updated date

--- src/test/java/com/realdolmen/course/domain/PassengerPersistenceTest.java
+++ src/test/java/com/realdolmen/course/domain/PassengerPersistenceTest.java
@@ -11,6 +11,12 @@ import static org.junit.Assert.assertSame;
 
 public class PassengerPersistenceTest extends PersistenceTest {
 
+    private Passenger createPassenger(){
+        LocalDate dob = LocalDate.of(1990,6,29);
+        Date dateOfBirth = Date.from(dob.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Passenger p = new Passenger("900629","Aveline","Estié",1500,new byte[5],PassengerType.OCCASIONAL, dateOfBirth, new Date());
+        return p;
+    }
     @Test
     public void passengerHasBeenPersisted() throws Exception {
         LocalDate dob = LocalDate.of(1990,6,29);
@@ -24,17 +30,23 @@ public class PassengerPersistenceTest extends PersistenceTest {
     @Test
     public void ageIsCorrectlyCalculated() throws Exception{
         // june 29, 1990 - turned 24
-        LocalDate dob = LocalDate.of(1990,6,29);
-        Date dateOfBirth = Date.from(dob.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Passenger p = new Passenger("900629","Aveline","Estié",1500,new byte[5],PassengerType.OCCASIONAL, dateOfBirth, new Date());
+        Passenger p = createPassenger();
         System.out.println(p.getAge() + "");
         assertSame(24, p.getAge());
 
         // december 31, 1990 - didn't turn 24 yet
-        dob = LocalDate.of(1990,12,31);
-        dateOfBirth = Date.from(dob.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        LocalDate dob = LocalDate.of(1990,12,31);
+        Date dateOfBirth = Date.from(dob.atStartOfDay(ZoneId.systemDefault()).toInstant());
         p = new Passenger("900629","Aveline","Estié",1500,new byte[5],PassengerType.OCCASIONAL, dateOfBirth, new Date());
         System.out.println(p.getAge() + "");
         assertSame(23, p.getAge());
+    }
+
+    @Test
+    public void timestampIsUpdated() throws Exception{
+        Passenger p = createPassenger();
+        assertSame(null, p.getDateLastUpdated());
+        entityManager().persist(p);
+        assertNotNull(p.getDateLastUpdated());
     }
 }
